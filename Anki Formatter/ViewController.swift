4ae4fb639162c16f45930c06193765c252bc55c5
Girish Palya <girishji@gmail.com>
2019-07-27 09:52:02 +0200

    Initial Commit

---
+++ Anki Formatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anki Formatter
+//
+//  Created by gpalya on 7/27/19.
+//  Copyright © 2019 gpalya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
